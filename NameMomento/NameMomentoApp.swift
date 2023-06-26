3b90220e3ce5fb616e4fed4d89a2a387263f1457
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-26 19:57:44 +0530

    Initial Commit

---
+++ NameMomento/NameMomentoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameMomentoApp.swift
+//  NameMomento
+//
+//  Created by Rishav Gupta on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct NameMomentoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
